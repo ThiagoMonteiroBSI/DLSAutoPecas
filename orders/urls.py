18e49466b9acdfb74eba0a15ad4f8e7f58c4d171
ThiagoMonteiroBSI <thiagodepaulamonteiro@gmail.com>
2026-05-25 17:40:54 -0300

    feat: autenticacao via token

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.OrderListView.as_view(), name='order-list'),
     path('checkout/', views.CheckoutView.as_view(), name='checkout'),
     path('shipping/simulate/', views.ShippingSimulationView.as_view(), name='shipping-simulate'), 
 ]
--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,7 @@ from .serializers import ShippingSimulationSerializer
 from rest_framework import generics
 from .models import Order
 from .serializers import OrderSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CheckoutView(generics.CreateAPIView):
     queryset = Order.objects.all()
@@ -29,3 +30,12 @@ class ShippingSimulationView(APIView):
             return Response(mock_shipping_options, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    from rest_framework.permissions import IsAuthenticated
+
+class OrderListView(generics.ListAPIView):
+
+    queryset = Order.objects.all().order_by('-created_at')
+    serializer_class = OrderSerializer
+    
+    permission_classes = [IsAuthenticated]
