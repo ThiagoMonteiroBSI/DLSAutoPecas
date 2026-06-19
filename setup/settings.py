f2a76cedd03f7d78309985964f0f3f11b580b341
ThiagoMonteiroBSI <thiagodepaulamonteiro@gmail.com>
2026-06-18 21:12:47 -0300

    feat: bloqueia CORS apenas para URLs do front-end

--- setup/settings.py
+++ setup/settings.py
@@ -136,4 +136,12 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:5173",
+    
+    TODO:
+    # "https://www.dlsautopecas.com.br",
+]
