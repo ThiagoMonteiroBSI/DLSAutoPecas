23d9f71b16cf879b8fc67184347d97eb6199e874
vencce <vitorferreira20005@gmial.com>
2026-06-24 13:49:45 -0300

    fix: alterando url do catalogo

--- setup/urls.py
+++ setup/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/catalog/', include('catalog.urls')),
+    path('api/v1/', include('catalog.urls')),
     path('api/v1/orders/', include('orders.urls')),
     path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
