d152ff771a536ec05ec3222d11cf2f9c23dd3543
vencce <vitorferreira20005@gmial.com>
2026-06-24 11:41:04 -0300

    fix: adiciona criação de superusuário no script de build

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # 3. Roda as migrações no banco de dados PostgreSQL do Render
 python manage.py migrate
+
+# 4. Cria um superusuário para acessar o Django Admin, caso ainda não exista, dados do no arquivo nas variaveis de ambiente do render
+python manage.py createsuperuser --noinput || true
